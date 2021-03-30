fb22ccdd75da146aae044a0e89d1ed3fb550456c
Valentin Raduti <valentin@thedotin.ro>
2021-03-30 10:51:05 +0300

    moved index

---
+++ index.ts
@@ -0,0 +1,9 @@
+export * from './src/chill-api/BootstrapConfig';
+export * from './src/chill-api/ChillAPIConfig';
+export * from './src/chill-api/GeneratorConfig';
+export * from './src/chill-api/UpdateConfig';
+
+export * from './src/delegate/Delegate';
+export * from './src/delegate/DelegateConfig';
+export * from './src/delegate/MethodDelegate';
+export * from './src/delegate/MethodDelegateConfig';
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './chill-api/BootstrapConfig';
-export * from './chill-api/ChillAPIConfig';
-export * from './chill-api/GeneratorConfig';
-export * from './chill-api/UpdateConfig';
-
-export * from './delegate/Delegate';
-export * from './delegate/DelegateConfig';
-export * from './delegate/MethodDelegate';
-export * from './delegate/MethodDelegateConfig';
