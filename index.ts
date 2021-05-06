5983b909d941bafdba7e246914bc797e3cbcef59
deroude <rvgabriel@yahoo.com>
2021-05-07 00:21:35 +0300

    forgot module loader

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from './src/chill-api/BootstrapConfig';
 export * from './src/chill-api/Config';
 export * from './src/chill-api/ModuleConfig';
+export * from './src/chill-api/ModuleLoader';
 
 export * from './src/delegate/Delegate';
 export * from './src/delegate/DelegateConfig';
