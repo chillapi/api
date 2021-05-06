7001f4a97140edb8e98d35e2745e7e107cb95c4f
deroude <rvgabriel@yahoo.com>
2021-05-07 00:10:40 +0300

    simplified further

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 export * from './src/chill-api/BootstrapConfig';
-export * from './src/chill-api/ChillAPIConfig';
-export * from './src/chill-api/GeneratorConfig';
-export * from './src/chill-api/UpdateConfig';
+export * from './src/chill-api/Config';
+export * from './src/chill-api/ModuleConfig';
 
 export * from './src/delegate/Delegate';
 export * from './src/delegate/DelegateConfig';
 export * from './src/delegate/MethodDelegate';
 export * from './src/delegate/MethodDelegateConfig';
-export * from './src/delegate/Persistence';
--- src/delegate/Persistence.ts
+++
@@ -1,7 +0,0 @@
-export interface Persistence {
-    beginTransaction(): Promise<void>;
-    commitTransaction(): Promise<void>;
-    rollbackTransaction(): Promise<void>;
-    executeQuery(context: any, params?: any[]): Promise<void>;
-    executeMigration(migrationSet: { id: string, apply: string, rollback: string }[]): Promise<void>;
-}
