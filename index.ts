85215588aaee1464060c322044cdac81e60f1d85
deroude <rvgabriel@yahoo.com>
2021-05-12 17:38:20 +0300

    added to index

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export * from './src/chill-api/BootstrapConfig';
 export * from './src/chill-api/Config';
 export * from './src/chill-api/ModuleConfig';
 export * from './src/chill-api/ModuleLoader';
+export * from './src/chill-api/Context';
 
 export * from './src/delegate/Delegate';
 export * from './src/delegate/DelegateConfig';
