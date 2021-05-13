7ee84a8ba735eb4507b17f96bdd79a5a9ed33fd2
deroude <rvgabriel@yahoo.com>
2021-05-13 23:07:28 +0300

    removed context from api

--- src/chill-api/Context.ts
+++
@@ -1,4 +0,0 @@
-export interface Context {
-    getComponent(name: string): any;
-    setComponent(name: string, comp: any): void;
-}
--- src/chill-api/ModuleLoader.ts
+++ src/chill-api/ModuleLoader.ts
@@ -1,6 +1,5 @@
-import { Context } from "./Context";
 import { ModuleConfig } from "./ModuleConfig";
 
 export interface ModuleLoader {
-    loadModule: (context: Context, config: ModuleConfig) => Promise<void>;
+    loadModule: (config: ModuleConfig) => Promise<any>;
 }
