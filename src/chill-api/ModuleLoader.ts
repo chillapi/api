1aec59cae1fa96bcd2f1e0e088545dfdbf3ede9d
deroude <rvgabriel@yahoo.com>
2021-05-13 22:55:13 +0300

    loadmodule as promise

--- src/chill-api/ModuleLoader.ts
+++ src/chill-api/ModuleLoader.ts
@@ -2,5 +2,5 @@ import { Context } from "./Context";
 import { ModuleConfig } from "./ModuleConfig";
 
 export interface ModuleLoader {
-    loadModule: (context: Context, config: ModuleConfig) => void;
+    loadModule: (context: Context, config: ModuleConfig) => Promise<void>;
 }
