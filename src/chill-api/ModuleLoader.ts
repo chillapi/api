52bbf9945c27166cdde86620eaed9b2148937048
deroude <rvgabriel@yahoo.com>
2021-05-14 21:48:26 +0300

    added root path

--- src/chill-api/ModuleLoader.ts
+++ src/chill-api/ModuleLoader.ts
@@ -3,5 +3,5 @@ import { OpenAPIV3 } from '../generated/openapi';
 
 export interface ModuleLoader {
     loadModule: (config: ModuleConfig) => Promise<any>;
-    generateStubs: (api: OpenAPIV3) => Promise<void>;
+    generateStubs: (api: OpenAPIV3, rootPath: string) => Promise<void>;
 }
