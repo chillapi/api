562f238abe3a888cabd38dd561473cd63477f7db
deroude <rvgabriel@yahoo.com>
2021-06-27 19:06:07 +0300

    moved index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from './chill-api/BootstrapConfig';
+export * from './chill-api/Config';
+export * from './chill-api/ModuleConfig';
+export * from './chill-api/ModuleLoader';
+
+export * from './delegate/Delegate';
+export * from './delegate/DelegateConfig';
+export * from './delegate/MethodDelegate';
+export * from './delegate/MethodDelegateConfig';
+
+export * from './generated/openapi';
+export * from './openapi/OpenAPIUtils';
