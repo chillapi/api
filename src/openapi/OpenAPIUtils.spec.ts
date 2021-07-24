96e7a50592b927898a4fc4a618bb80f04e1edd0c
deroude <rvgabriel@yahoo.com>
2021-07-24 21:22:33 +0300

    added operations to api utils

--- src/openapi/OpenAPIUtils.spec.ts
+++ src/openapi/OpenAPIUtils.spec.ts
@@ -1,5 +1,5 @@
 import { OpenAPIV3 } from '../generated/openapiv3';
-import { loadEntities } from './OpenAPIUtils';
+import { loadEntities, loadOperations } from './OpenAPIUtils';
 const api: OpenAPIV3 = {
     openapi: '3.0.1',
     info: {
@@ -7,10 +7,211 @@ const api: OpenAPIV3 = {
         version: '1.0'
     },
     paths: {
+        "/article": {
+            "get": {
+                "tags": [
+                    "article"
+                ],
+                "summary": "Get all articles",
+                "operationId": "getArticles",
+                "parameters": [
+                    {
+                        "in": "query",
+                        "name": "category",
+                        "schema": {
+                            "type": "string",
+                            "default": "BLOG"
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "filter",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "language",
+                        "schema": {
+                            "type": "string",
+                            "default": "en"
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "page",
+                        "schema": {
+                            "type": "integer",
+                            "default": 0
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "itemsPerPage",
+                        "schema": {
+                            "type": "integer",
+                            "default": 20
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Articles retrieved",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/Article"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                "security": [
+                    {
+                        "auth": [
+                            "openid"
+                        ]
+                    }
+                ]
+            },
+            "post": {
+                "tags": [
+                    "article"
+                ],
+                "summary": "Add an article",
+                "operationId": "addArticle",
+                "requestBody": {
+                    "description": "Article object that needs to be added",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Article"
+                            }
+                        }
+                    },
+                    "required": true
+                },
+                "responses": {
+                    "200": {
+                        "description": "Article created",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "$ref": "#/components/schemas/Article"
+                                }
+                            }
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid object supplied",
+                        "content": {}
+                    }
+                },
+                "security": [
+                    {
+                        "auth": [
+                            "openid"
+                        ]
+                    }
+                ]
+            }
+        },
+        "/article/{id}": {
+            "put": {
+                "tags": [
+                    "article"
+                ],
+                "summary": "Modify an article",
+                "operationId": "updateArticle",
+                "parameters": [
+                    {
+                        "in": "path",
+                        "required": true,
+                        "name": "id",
+                        "schema": {
+                            "type": "string",
+                            "format": "uuid"
+                        }
+                    }
+                ],
+                "requestBody": {
+                    "description": "Article that is updated",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Article"
+                            }
+                        }
+                    }
+                },
+                "responses": {
+                    "200": {
+                        "description": "Article updated",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "$ref": "#/components/schemas/Article"
+                                }
+                            }
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid object supplied"
+                    },
+                    "404": {
+                        "description": "Article not found"
+                    }
+                },
+                "security": [
+                    {
+                        "auth": [
+                            "openid"
+                        ]
+                    }
+                ]
+            },
+            "delete": {
+                "tags": [
+                    "article"
+                ],
+                "summary": "Delete an article",
+                "operationId": "deleteArticle",
+                "parameters": [
+                    {
+                        "in": "path",
+                        "required": true,
+                        "name": "id",
+                        "schema": {
+                            "type": "string",
+                            "format": "uuid"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Article deleted"
+                    },
+                    "404": {
+                        "description": "Article not found"
+                    }
+                },
+                "security": [
+                    {
+                        "auth": [
+                            "openid"
+                        ]
+                    }
+                ]
+            }
+        }
     },
     components: {
         schemas: {
-            MyEntity: {
+            Article: {
                 properties: {
                     id: {
                         type: 'integer'
@@ -27,5 +228,25 @@ const api: OpenAPIV3 = {
 test('loads entities from OpenAPI document', () => {
     const entities = loadEntities(api);
     expect(entities.length).toBe(1);
-    expect(entities[0].name).toBe('MyEntity');
+    expect(entities[0].name).toBe('Article');
 });
+
+test('loads operations from OpenAPI document', () => {
+    const operations = loadOperations(api);
+    expect(operations.length).toBe(4);
+    expect (operations[0].path).toBe("/article");
+    expect(operations[0].method).toBe('get');
+    expect(operations[0].queryParameters.length).toBe(5);
+    expect(operations[0].responses[0].code).toBe(200);
+    expect(operations[0].responses[0].isDefault).toBeTruthy();
+    expect(operations[0].responses[0].isError).toBeFalsy();
+    expect(operations[0].responses[0].content.isArray).toBeTruthy();
+    expect(operations[0].responses[0].content.type).toBe('Article');
+    expect(operations[0].responses[0].content.isReference).toBeTruthy();
+    expect(operations[2].method).toBe('put');
+    expect(operations[2].pathParameters[0].name).toBe('id');
+    expect(operations[2].body.type).toBe('Article');
+    expect(operations[2].body.isReference).toBeTruthy();
+    expect(operations[2].responses[1].code).toBe(400);
+    expect(operations[2].responses[1].isError).toBeTruthy();
+})
--- src/openapi/OpenAPIUtils.ts
+++ src/openapi/OpenAPIUtils.ts
@@ -1,21 +1,46 @@
-import { OpenAPIV3 } from "../generated/openapiv3";
+import { OpenAPIV3, Operation as OpenAPIOperation } from "../generated/openapiv3";
 
 const refPattern = /^#\/components\/schemas\/(.+)$/;
 const batchPathPattern = /^\/([^\/]+)$/;
 const idPathPattern = /^\/([^\/]+)\/\{id\}$/
 
-export interface Property {
-    name: string;
+export type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+export interface Property extends Parameter {
     isId: boolean;
-    type: string;
-    description?: string;
-    isRequired: boolean;
-    reference?: string;
 }
 
 export interface Entity {
     name: string;
     properties: Property[];
+}
+
+export interface Parameter {
+    name: string;
+    type: string;
+    description?: string;
+    isRequired: boolean;
+    isReference: boolean;
+    isArray: boolean;
+}
+
+export interface Response {
+    code: number;
+    isDefault: boolean;
+    isError: boolean;
+    content: Parameter;
+}
+
+export interface Operation {
+    path: string;
+    id: string;
+    description?: string;
+    method: HttpMethod;
+    queryParameters?: Parameter[];
+    pathParameters?: Parameter[];
+    body?: Parameter;
+    responses: Response[];
+    authorizationScopes?: string[]
 }
 
 export function reference(prop: any): string {
@@ -35,12 +60,8 @@ export function loadEntities(api: OpenAPIV3): Entity[] {
         const entity: Entity = { name: k, properties: [] };
         for (const [pk, pv] of Object.entries(schema.properties)) {
             entity.properties.push({
-                name: pk,
-                isId: pk.toLowerCase() === 'id',
-                isRequired: schema.required && schema.required.indexOf(pk.toLowerCase()) > 0,
-                type: pv.format ? `${pv.type}:${pv.format}` : pv.type,
-                reference: reference(pv),
-                description: pv.description
+                ...param(pk, pv, schema.required && schema.required.indexOf(pk.toLowerCase()) > 0),
+                isId: pk.toLowerCase() === 'id'
             })
         }
         entities.push(entity);
@@ -54,8 +75,8 @@ export function loadEntities(api: OpenAPIV3): Entity[] {
         for (let i = 0; i < entities.length; i++) {
             const e = entities[i];
             for (const prop of e.properties) {
-                if (prop.reference) {
-                    const si = entities.findIndex(o => o.name === prop.reference);
+                if (prop.isReference) {
+                    const si = entities.findIndex(o => o.name === prop.type);
                     if (si > i) {
                         sw = true;
                         const oe: Entity = entities[si];
@@ -74,3 +95,51 @@ export function loadEntities(api: OpenAPIV3): Entity[] {
 
     return entities;
 }
+
+export function loadOperations(api: OpenAPIV3): Operation[] {
+    const re: Operation[] = [];
+    for (const [path, ops] of Object.entries(api.paths)) {
+        for (const [method, op] of Object.entries(ops)) {
+            const opC = (op as OpenAPIOperation);
+
+            re.push({
+                path,
+                method: method as HttpMethod,
+                id: opC.operationId,
+                pathParameters: opC.parameters?.filter(p => p.in === 'path').map(p => param(p.name as string, p.schema, p.required as boolean)),
+                queryParameters: opC.parameters?.filter(p => p.in === 'query').map(p => param(p.name as string, p.schema, p.required as boolean)),
+                body: opC.requestBody ? {
+                    ...param(null, opC.requestBody?.content && (opC.requestBody?.content as any)['application/json'] && (opC.requestBody?.content as any)['application/json'].schema, !!opC.requestBody?.required),
+                    description: opC.requestBody?.description
+                } : null,
+                responses: Object.entries(opC.responses).map(([code, res]) => ({
+                    code: Number(code),
+                    isDefault: Number(code) < 300,
+                    isError: Number(code) >= 400,
+                    description: res.description,
+                    content: { ...param(null, res?.content && res?.content['application/json'] && res?.content['application/json'].schema), description: res.description }
+                }))
+            });
+        }
+    }
+    return re;
+}
+
+function param(name: string, content: any, isRequired?: boolean): Parameter {
+    if (!content) {
+        return null;
+    }
+    if (content.type === 'array') {
+        return { ...param(name, content.items, isRequired), isArray: true };
+    }
+    const ref = reference(content);
+    return {
+        name,
+        type: ref || (content.format ? `${content.type}:${content.format}` : content.type),
+        isReference: !!ref,
+        description: content.description,
+        isRequired: !!isRequired,
+        isArray: false
+    };
+}
+
