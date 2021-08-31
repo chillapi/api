0698d8662e0a50e2cb0cc048af22e7a4581b57ff
deroude <rvgabriel@yahoo.com>
2021-08-31 15:06:06 +0300

    and export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export * from './chill-api/Config';
 export * from './chill-api/ModuleConfig';
-
 export * from './delegate/Delegate';
 export * from './delegate/DelegateConfig';
+export * from './delegate/DelegateFactory';
 export * from './delegate/MethodDelegate';
 export * from './delegate/MethodDelegateConfig';
-
 export * from './openapi/OpenAPIUtils';
-
 export * from './template/TemplateUtils';
+
+
+
